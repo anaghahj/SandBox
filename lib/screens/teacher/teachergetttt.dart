fe097cea71287821d83603456f28658149e492f6
anaghahj <anagha.jutoor@gmail.com>
2024-04-26 20:57:08 +0530

    first commit

--- lib/screens/teacher/teachergetttt.dart
+++ lib/screens/teacher/teachergetttt.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+final String ip = "192.168.0.141";
+
 Future<List<dynamic>> fetchDatafoi(String usn) async {
-  var url = Uri.parse('https://10.10.0.10/dbms_student/teacher_foi.php');
+  var url = Uri.parse('https://$ip/dbms_student/teacher_foi.php');
   var response = await http.post(url, body: {'Teacher_id': usn});
   var data = jsonDecode(response.body);
   print("Data: $data");
@@ -12,14 +14,13 @@ Future<List<dynamic>> fetchDatafoi(String usn) async {
 
 Future<List> getDatainfo() async {
   final response =
-      await http.get(Uri.parse("https://10.10.0.10/dbms_student/teacher.php"));
+      await http.get(Uri.parse("https://$ip/dbms_student/teacher.php"));
   print(jsonDecode(response.body));
   return jsonDecode(response.body);
 }
 
 Future<List<dynamic>> fetchDataprojt(String usn) async {
-  var url =
-      Uri.parse('https://192.168.0.141/dbms_student/teacher_projects.php');
+  var url = Uri.parse('https://$ip/dbms_student/teacher_projects.php');
   var response = await http.post(url, body: {'Teacher_id': usn});
   var data = jsonDecode(response.body);
   print("Data: $data");
